749dfec66e47c51542758b3ed5c93975cbacf151
mattpicone <matt.picone@icloud.com>
2026-08-31 11:56:59 -0400

    support secure nonce generation in workerd

--- src/uncrypto-worker.ts
+++ src/uncrypto-worker.ts
@@ -7,7 +7,17 @@ const workerCrypto = crypto;
 
 export const subtle = workerCrypto.subtle;
 export const randomUUID: Crypto["randomUUID"] = () => workerCrypto.randomUUID();
-export const getRandomValues: Crypto["getRandomValues"] = (array) =>
-  workerCrypto.getRandomValues(array);
+export const getRandomValues: Crypto["getRandomValues"] = (array) => {
+  const bytes = new Uint8Array(array.buffer, array.byteOffset, array.byteLength);
+  let offset = 0;
+  while (offset < bytes.length) {
+    const random = workerCrypto.randomUUID().replaceAll("-", "");
+    for (let index = 0; index < random.length && offset < bytes.length; index += 2) {
+      bytes[offset] = Number.parseInt(random.slice(index, index + 2), 16);
+      offset += 1;
+    }
+  }
+  return array;
+};
 
 export default { subtle, randomUUID, getRandomValues };
